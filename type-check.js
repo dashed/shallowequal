73b0c14f35ab1f4cc36394274c8cd3a1d4f6f4ce
Alberto Leal <mail4alberto@gmail.com>
2019-01-13 17:37:50 -0500

    upgrade flow to v0.90 and add flow type checking

---
+++ type-check.js
@@ -0,0 +1,40 @@
+// @flow
+
+import shallowequal from "./index.original.js";
+
+shallowequal("a", "b");
+shallowequal();
+
+// $ExpectError
+shallowequal("a", "b", false);
+
+// $ExpectError
+shallowequal("a", "b", 1);
+
+// $ExpectError
+shallowequal("a", "b", "");
+
+shallowequal("a", "b", null);
+
+shallowequal("a", "b", void 0);
+
+// this is legal because function returns void 0
+shallowequal("a", "b", () => {});
+
+shallowequal("a", "b", (): boolean => {
+  return true;
+});
+
+shallowequal("a", "b", (x, y, z: ?string): boolean => {
+  return true;
+});
+
+// $ExpectError
+shallowequal("a", "b", (x, y, z: ?number): boolean => {
+  return true;
+});
+
+// $ExpectError
+shallowequal("a", "b", (): number => {
+  return 42;
+});
